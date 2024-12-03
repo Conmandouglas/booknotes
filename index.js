0928060df32c6d011430251d61ce338f18b3daf9
Conmandouglas <conmandouglas@gmail.com>
2024-12-03 15:45:25 -0600

    add ejs, change port, create and edit review

--- index.js
+++ index.js
@@ -2,14 +2,94 @@ import express from 'express';
 import pg from 'pg';
 
 const app = express();
-const port = 4000;
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "bookreviews", 
+  password: "ConnorD:0124",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render(index.js);
+let reviews = [];
+async function checkReviews() {
+  // selecting reviews id, title, detils and user name (referenced as author)
+  // FROM reviews r is the table we are retrieving data from
+  // JOIN users u means we are joining with users and users is alised as u
+  // ON is the condition, only is review user id is equal to users id
+  // order by sorts by id in reviews table NEWEST first
+  const result = await db.query(
+    `SELECT r.id, r.title, r.details, u.name AS author 
+     FROM reviews r 
+     JOIN users u ON r.userid = u.id 
+     ORDER BY r.id DESC;`
+  );
+  return result.rows;
+}
+
+app.get('/', async (req, res) => {
+  try {
+    // awaits data from function that gets the reviews
+    const reviewsList = await checkReviews();
+    // renders all reviews currently in database
+    res.render("index.ejs", {
+      listUser: "Connor",
+      list: reviewsList,
+      title: "Book Reviews"
+    })
+  } catch (err) {
+    console.log("Error fetching items:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+app.get('/add', async (req, res) => {
+  res.render("modify.ejs", {
+    title: "Write a Review"
+  });
+})
+
+app.get('/edit/:id', async (req, res) => {
+  const reviewId = req.params.id;
+
+  try {
+    const result = await db.query(
+      'SELECT * FROM reviews WHERE id = $1', 
+      [reviewId]);
+    const review = result.rows[0];
+
+    res.render('modify.ejs', {
+      review: review,
+      title: 'Edit Review'
+    });
+  } catch (err) {
+    console.error('Error fetching review:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/edit/:id', async (req, res) => {
+  const reviewId = req.params.id;
+  const { title, details } = req.body;
+
+  try {
+    await db.query(
+      'UPDATE reviews SET title = $1, details = $2 WHERE id = $3',
+      [title, details, reviewId]
+    );
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error updating reviews:', err);
+    res.status(500).send('Internal Server Error');
+  }
+})
 
 app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`);
--- todo.md
+++ todo.md
@@ -1,3 +1,8 @@
+MINDSET TO KEEP:
+skill comes with time. keep at it, stay organized, and stay focused.
+nothing worth showing off ever comes easy.
+one year ago: I was at the very start of the course, at papaws, after watching Wonka.
+
 Monday, Dec 2
 - I finished up some final freeform drawings and scrapped draw.io
 - I set up the project & file structure
@@ -5,3 +10,31 @@ Monday, Dec 2
 - I set up the data structure in PG admin
 To do tommorow:
 - get basic website to show database review data on home page
+
+Tuesday, Dec 3
+- Added EJS package, index ejs file, as well as partials
+- Updated port in index.js to port 3000
+- Added the database initilization in index.js
+- Created function that grabs all reviews, puts in array
+- Added home "/" route that renders reviews on index.ejs
+
+
+TODO (in order):
+- Add a review create screen
+ - Title, details, rating
+ - Automatically add the author and the date & time created
+ - Fetch the book cover API for the image
+- Update project to include multi-user support
+- Add choose-user route and page
+- Add ability to edit and delete reviews
+- Add ability to show all reviews, or just the users own
+- Only allow user to delete review if they wrote it
+- Add a confirmation diologue before deleting a review
+- Use amazon API to auto add a link to search the book on amazon
+- Test & make sure project works up to this point
+- Start doing some basic styling, and decide whether it will be worth it
+  to do bootstrap
+- Add a light/dark toggle in a corner
+- Find a way to add an arrow that pops up to scroll back up to the top
+- Pat myself on the back for accomplishing this task of a project, my FINAL
+capstone is now complete.
