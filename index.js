968f6ac0bd5c48fafe4d30be44c9936409be0d09
Conmandouglas <conmandouglas@gmail.com>
2024-12-13 19:52:08 -0600

    add auto generate book thumbnail from api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import {} from 'dotenv/config';
 import express from 'express';
 import pg from 'pg';
+import axios from 'axios'
 
 const app = express();
-const port = 3000;
+const portRoute = 3000;
 
 const db = new pg.Client({
   user: "postgres",
@@ -12,7 +14,15 @@ const db = new pg.Client({
   port: 5432,
 });
 
-db.connect();
+
+const dbConnected = await db.connect().then(()=>true).catch(err => console.log(err))
+
+app.listen(portRoute, () => {
+  if(!dbConnected) {
+    return console.log(`The application could not be started because the database is either offline or the connection information provided is invalid. Check the logs for details.`)
+  }
+  console.log(`App is running on ${portRoute}`);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
@@ -26,7 +36,7 @@ async function checkReviews() {
   // ON is the condition, only is review user id is equal to users id
   // order by sorts by id in reviews table NEWEST first
   const result = await db.query(
-    `SELECT r.id, r.title, r.details, u.name AS author 
+    `SELECT r.id, r.title, r.details, r.rating, r.date, r.book_auth, r.thumb, u.name AS author 
      FROM reviews r 
      JOIN users u ON r.userid = u.id 
      ORDER BY r.id DESC;`
@@ -35,19 +45,80 @@ async function checkReviews() {
 }
 
 app.get('/', async (req, res) => {
-  try {
-    // awaits data from function that gets the reviews
-    const reviewsList = await checkReviews();
-    // renders all reviews currently in database
-    res.render("index.ejs", {
-      listUser: "Connor",
-      list: reviewsList,
-      title: "Book Reviews"
-    })
-  } catch (err) {
-    console.log("Error fetching items:", err);
-    res.status(500).send("Internal Server Error");
+// what this needs to do:
+// go through the openlibary API, get the book, then get the data, and cover API url for reach review in
+// the result array
+// what is happening:
+// 
+
+  const result = await checkReviews();
+
+  for  (const review of result) {
+    const title = review.title;
+    const id = review.id;
+
+    // check if whatever current item, and match it with reviews in database, see if it has link
+    // if it does not have image link, then grab one from API and add it to column
+    // after going through all books, then it will be done
+    // if no image, make a special image show, a default one
+
+    // review.id = id in table
+    const checkThumbQuery = `
+      SELECT thumb
+      FROM reviews
+      WHERE id = $1;
+      `;
+
+    const checkThumbResult = await db.query(checkThumbQuery, [id]);
+
+    const currentThumb = checkThumbResult.rows[0]?.thumb;
+    // then check if it has data in the thumb column
+    if (!currentThumb) {
+      let SEARCH_API_URL = process.env.SEARCH_API_URL;
+      let COVER_ID_API_URL = process.env.COVER_ID_API_URL;
+      let COVER_ISBN_API_URL = process.env.COVER_ISBN_API_URL;
+
+      const searchTitle = title.replaceAll(' ', '+');
+      SEARCH_API_URL = `${SEARCH_API_URL}${searchTitle}`.toLowerCase();
+
+      const response = await axios.get(SEARCH_API_URL).catch(err => {
+        console.log(err); return null;
+      });
+
+      if(!response || !response.data.docs || response.data.docs.length === 0) {
+        console.log(`No data found for: ${title}`);
+        continue;
+      } 
+
+      const bookData = response.data.docs[0];
+      const cover_id = bookData?.cover_i;
+      const isbn_data = bookData?.isbn[0];
+
+      const COVER_API_URL = isbn_data
+        ? COVER_ISBN_API_URL.replace('isbn_data', isbn_data)
+        : COVER_ID_API_URL.replace('cover_id', cover_id);
+
+      console.log(`Cover URL for ${title}: ${COVER_API_URL}`);
+
+      // update thumb column in database
+      const updateThumbQuery = `
+        UPDATE reviews
+        SET thumb = $1
+        WHERE id = $2;
+      `;
+      await db.query(updateThumbQuery, [COVER_API_URL, id]);
+    } else {
+      console.log(`Thumb already exists for ${title}. Skipping API call.`);
+    }
+
   }
+
+  const reviewsList = await checkReviews();
+  res.render("index.ejs", {
+    listUser: "Connor",
+    list: reviewsList,
+    title: "Book Reviews",
+  });
 });
 
 app.get('/add', async (req, res) => {
@@ -58,16 +129,22 @@ app.get('/add', async (req, res) => {
 
 app.post('/add', async (req, res) => {
   try {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    let currentDate = `${month}/${day}/${year}`
     const result = await db.query(
-      'INSERT INTO reviews (title, details, userid) VALUES ($1, $2, $3)',
-      [req.body.title, req.body.details, 1]
+      'INSERT INTO reviews (title, details, userid, rating, date, book_auth) VALUES ($1, $2, $3, $4, $5, $6)',
+      [req.body.title, req.body.details, 1, req.body.rating, currentDate, book_auth]
       // TO DO: 1 updates based on what user is selected
     );
+
     res.redirect('/');
   } catch (err) {
     console.log(err);
   }
-})
+});
 
 app.get('/edit/:id', async (req, res) => {
   const reviewId = req.params.id;
@@ -96,8 +173,8 @@ app.post('/edit/:id', async (req, res) => {
   try {
     // TO DO: only go through if post userid is equal to current userid
     await db.query(
-      'UPDATE reviews SET title = $1, details = $2 WHERE id = $3',
-      [title, details, reviewId]
+      'UPDATE reviews SET title = $1, details = $2, book_auth = $3, rating = $4 WHERE id = $5',
+      [title, details, book_auth, rating, reviewId]
     );
     res.redirect('/');
   } catch (error) {
@@ -106,6 +183,6 @@ app.post('/edit/:id', async (req, res) => {
   }
 })
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`);
-});
+});*/
--- todo.md
+++ todo.md
@@ -31,12 +31,25 @@ To do tommorow:
   - use float temporarily before main styling
 - Add choose user route & page
 
+Wednesday Dec 4
+- Add rating number input box to modify.ejs
+- Add rating column to reviews table
+- Add "date" posted column to database
+- Add date posted to each post upon creation
+- Install axios
+- Add functioning book cover API that grabs books based on title entered
+
+Thursday Dec 5
+- Progress on transferring over to open library covers
+
+Friday Dec 13
+- Today, after DAYS of hard work, I finally got the book covers rendering on screen
+To do on Sunday:
+- Add multi user support
+- Add the choose-user route and page
+
 
 TODO (in order):
-- Add a review create screen
- - Title, details, rating
- - Automatically add the author and the date & time created
- - Fetch the book cover API for the image
 - Update project to include multi-user support
 - Add choose-user route and page
 - Add ability to edit and delete reviews
@@ -49,5 +62,6 @@ TODO (in order):
   to do bootstrap
 - Add a light/dark toggle in a corner
 - Find a way to add an arrow that pops up to scroll back up to the top
+- Override the auto thumbnail and add a way for user to upload custom photo for book using URL or ISBN code and it grabs it and updates it in database
 - Pat myself on the back for accomplishing this task of a project, my FINAL
-capstone is now complete.
+capstone is now complete. But the journey doesn't end here.
